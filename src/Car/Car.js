ae7514f7b8c817b6d723e650cc0059c638bb12ac
DayOut <fearess7@gmail.com>
2019-09-19 18:05:17 +0300

    Created first element 'Car'

---
+++ src/Car/Car.js
@@ -0,0 +1,24 @@
+import React from 'react';
+export default (props) => (
+    <div>
+        <h2>Component!</h2>
+        <strong>{props.text}</strong>
+    </div>
+);
+
+// function Car(){
+//     return (
+//         <h2>Component!</h2>
+//     );
+// }
+
+// const car = () => {
+//     return (
+//         <h2>Component!</h2>
+//     );
+// }
+
+// const car = () => <h2>Component!</h2>
+
+// export default car
+
